df1e95f9c94c4882ad5c258fe4e1a7a43c9be57b
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-09-26 20:10:28 +0900

    dd

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} style={{width:"393px", height:"100%", margin:"0 auto", backgroundColor:"lightblue"}}>
         {children}
       </body>
     </html>
